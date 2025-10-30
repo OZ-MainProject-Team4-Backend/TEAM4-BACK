6bcf81643d9d9698a577d7be8c85ef15bbb805c6
Yong-back <skathd6406@gmail.com>
2025-10-30 16:39:56 +0900

    feat : ai_model_settings / ai_chat_logs 모델 생성

--- chatbot/utils.py
+++ chatbot/utils.py
@@ -1,22 +1,23 @@
 import os
 
 from openai import OpenAI
-from openai.types.chat import ChatCompletionMessageParam, ChatCompletion
+from openai.types.chat import ChatCompletion, ChatCompletionMessageParam
 
 client = OpenAI(api_key=os.getenv("GPT_API_KEY"))
 
 
 def ask_gpt(prompt: str) -> str:
-   from typing import List
-   messages : List[ChatCompletionMessageParam] = [ # type: ignore[assignment]
-       {"role": "system", "content": "너는 날씨 기반 패션 코디 추천 봇이야."},
-       {"role": "user", "content": prompt},
-   ]
+    from typing import List
 
-   response: ChatCompletion = client.chat.completions.create(
-       model = "gpt-4o",
-       messages = messages,
-       temperature = 1.0,
-   )
+    messages: List[ChatCompletionMessageParam] = [  # type: ignore[assignment]
+        {"role": "system", "content": "너는 날씨 기반 패션 코디 추천 봇이야."},
+        {"role": "user", "content": prompt},
+    ]
 
-   return response.choices[0].message.content.strip()
+    response: ChatCompletion = client.chat.completions.create(
+        model="gpt-4o",
+        messages=messages,
+        temperature=1.0,
+    )
+
+    return response.choices[0].message.content.strip()
