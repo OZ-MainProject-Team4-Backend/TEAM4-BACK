88df9c49dc08d6941ebbcb7d6de40889dd495543
Yong-back <skathd6406@gmail.com>
2025-10-30 18:02:39 +0900

    feat : chat _log 기능 구햔

--- apps/urls.py
+++ apps/urls.py
@@ -17,12 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    # chat
-    path("api/chat/", include("apps.chat.urls")),
-from django.urls import path
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -32,6 +26,9 @@ from drf_spectacular.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    # chat
+    path("api/chat/", include("apps.chat.urls")),
+    # swagger
     path(
         "api/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
