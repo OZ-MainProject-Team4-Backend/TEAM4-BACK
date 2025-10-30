97ddee8badc5516c3c64e76aea04660ddaba4cb0
Yong-back <skathd6406@gmail.com>
2025-10-30 17:42:24 +0900

    feat: chat_log 로직 구현

---
+++ apps/chatbot/serializer.py
@@ -0,0 +1,22 @@
+from apps.chatbot.models import AiChatLogs
+from rest_framework import serializers
+
+
+class AiChatLogReadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AiChatLogs
+        fields = [
+            "id",
+            "session_id",
+            "user_question",
+            "ai_answer",
+            "context",
+            'created_at'
+        ]
+
+class SessionSummarySerializer(serializers.Serializer):
+    session_id = serializers.UUIDField()
+    last_message_at = serializers.DateTimeField()
+    message_count = serializers.IntegerField()
+    last_question = serializers.CharField(allow_blank=True)
+    last_answer = serializers.CharField(allow_blank=True)
