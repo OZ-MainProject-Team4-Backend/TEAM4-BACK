f57d1fa84e211d60844b1a69dc205d18bd0608fc
moxopills <moxopills@gmail.com>
2025-10-30 15:33:43 +0900

    users.models

--- apps/settings.py
+++ apps/settings.py
@@ -35,14 +35,13 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # user
-    'apps.users'
+    'users',
     # JWT
     "rest_framework",
     "rest_framework_simplejwt",
