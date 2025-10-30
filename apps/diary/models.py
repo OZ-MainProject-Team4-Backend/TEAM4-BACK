c7c365f104db05e8cd25cc3862acfa9a25c10617
romiii35 <romiii9735@gmail.com>
2025-10-30 16:32:00 +0900

    main/urls 머지완료

---
+++ apps/diary/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+from AWS.users.models import User
+from AWS.weather.models import WeatherData
+
+
+class Diary(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    weather_data = models.ForeignKey(WeatherData, on_delete=models.CASCADE)
+    satisfaction = models.IntegerField()
+    title = models.CharField(max_length=255)
+    notes = models.TextField()
+    image_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'diary'
+        verbose_name = 'Diary'
+        verbose_name_plural = 'Diaries'
+
+    def __str__(self):
+        return str(self.date)
+
+    def delete(self, using=None, keep_parents=False):
+        self.deleted_at = timezone.now()
+        self.save()
