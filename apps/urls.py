2d3179fed0d272e06aa752f1a2bacd98dcf540b6
kang9805 <zzionkk@naver.com>
2025-10-31 18:17:39 +0900

    feat: location 앱 URL 연결 및 엔드포인트 등록

--- apps/urls.py
+++ apps/urls.py
@@ -43,4 +43,5 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/users/", include("apps.users.urls", namespace="users")),
+    path("api/location/", include("apps.locations.urls")),
 ]
