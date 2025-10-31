0ab97ef2ee5efadb28c657ebfa73443ede355d25
moxopills <moxopills@gmail.com>
2025-10-31 16:53:04 +0900

    맵핑

--- apps/urls.py
+++ apps/urls.py
@@ -22,6 +22,7 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,4 +40,7 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/users/", include("apps.users.urls", namespace="users")),
 ]
