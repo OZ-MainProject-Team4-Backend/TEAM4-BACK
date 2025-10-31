8911dc90e86228c87380c982e1b18a6852039767
kang9805 <zzionkk@naver.com>
2025-10-31 15:28:36 +0900

    feat: Location, FavoriteLocation serializers 추가(수정)

--- apps/locations/serializers.py
+++ apps/locations/serializers.py
@@ -1,18 +1,21 @@
-from apps.locations.models import Location, FavoriteLocation
 from rest_framework import serializers
+
+from apps.locations.models import FavoriteLocation, Location
 
 
 class LocationSerializer(serializers.ModelSerializer):
     """위치 정보 직렬화"""
+
     class Meta:
         model = Location
         fields = ["id", "city", "district", "latitude", "longitude"]
 
+
 class FavoriteLocationSerializer(serializers.ModelSerializer):
     """즐겨찾기 위치 직렬화"""
+
     location = LocationSerializer(read_only=True)
 
     class Meta:
         model = FavoriteLocation
         fields = ["id", "alias", "is_default", "location", "created_at"]
-
